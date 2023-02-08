05c38f68678d98b343a8181f002dee1aaa026dc8
Pavelingus <zarovnpasha@mail.ru>
2023-02-08 14:41:07 +0300

    img preloader on app instead of main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import AppRouter from './components/Main Page/AppRouter';
 import NavBar from './components/Main Page/NavBar';
 import GitHubModal from './components/UI/GitHubModal';
 import GitModalContent from './components/UI/GitModalContent';
+import logo1 from './assets/chessOverlay.jpg'
+import logo2 from './assets/tablegame1.jpg'
+import useImagePreloader from "./hooks/imagePreloader";
 
 
 
@@ -12,21 +15,28 @@ function App() {
 
   const [visible, setVisible] = useState(false);
 
+  const {imagesPreloaded} = useImagePreloader([logo1, logo2])
+
 
   return (
     <>
-      <GitHubModal
-        visible = {visible}
-        setVisible = {setVisible}
-        >
-          <GitModalContent
-            setVisible = {setVisible}
+      {imagesPreloaded 
+      &&
+      <>     
+        <GitHubModal
+          visible = {visible}
+          setVisible = {setVisible}
+          >
+            <GitModalContent
+              setVisible = {setVisible}
+            />
+        </GitHubModal>
+        <NavBar
+          setVisible = {setVisible}
           />
-      </GitHubModal>
-      <NavBar
-        setVisible = {setVisible}
-        />
-      <AppRouter/>
+        <AppRouter/>
+      </>     
+      }
     </>
   )
 }
--- src/components/Main Page/MainPage.tsx
+++ src/components/Main Page/MainPage.tsx
@@ -1,27 +1,20 @@
 import { useRef} from "react";
 import GamesSection from "./GamesSection";
 import Header from "./Header";
-import logo1 from '../../assets/chessOverlay.jpg'
-import logo2 from '../../assets/tablegame1.jpg'
-import useImagePreloader from "../../hooks/imagePreloader";
+
 
 const MainPage = () => {
   const divRef = useRef<HTMLDivElement>(null!);
-  const {imagesPreloaded} = useImagePreloader([logo1, logo2])
 
 
   function clickHandler() {
     divRef.current.scrollIntoView({ behavior: "smooth" });
   }
 
-  return (
+  return ( 
     <>
-      {imagesPreloaded &&    
-      <>
-        <Header clickHandler = {clickHandler} />
-        <GamesSection ref = {divRef} />
-      </>
-      }
+      <Header clickHandler = {clickHandler} />
+      <GamesSection ref = {divRef} />
     </>
   );
 };
