7aa10c9957593e53b87ab1765bb782e7c8888239
Pavelingus <zarovnpasha@mail.ru>
2023-02-28 16:14:52 +0300

    creating a portal for a modal

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import GitModalContent from './components/UI/GitModalContent';
 import logo1 from './assets/chessOverlay.jpg';
 import logo2 from './assets/tablegame1.jpg';
 import useImagePreloader from "./hooks/imagePreloader";
+import ReactDOM from 'react-dom';
 
 
 
@@ -17,20 +18,27 @@ function App() {
 
   const { imagesPreloaded } = useImagePreloader([logo1, logo2]);
 
+  const ModalComponent = () => {
+    return (
+      <GitHubModal
+        visible={visible}
+        setVisible={setVisible}
+      >
+        <GitModalContent
+          setVisible={setVisible}
+        />
+      </GitHubModal>
+    );
+  };
+
+  const ModalPortal = ReactDOM.createPortal(<ModalComponent />, document.querySelector('#modalRoot') as HTMLElement);
 
   return (
     <>
       {imagesPreloaded
         &&
         <>
-          <GitHubModal
-            visible={visible}
-            setVisible={setVisible}
-          >
-            <GitModalContent
-              setVisible={setVisible}
-            />
-          </GitHubModal>
+          {ModalPortal}
           <NavBar
             setVisible={setVisible}
           />
